dbf8380803c01fd43bc051cc1f7ec80247de64c6
BuiChiHuong <67896751+BuiChiHuong@users.noreply.github.com>
2020-07-08 17:35:58 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// function binhPhuong(n){
+// return n*n;
+// }
+// console.log(binhPhuong(6));
+// function chuVi(a,b){
+//     return (a+b)*2;
+// }
+// console.log('chu vi hinh chu nhat: ' + chuVi(7,8));
+function bangCuChuong(n){
+    console.log('bang cuu chuong cua so ' + n);
+    for(var i=1; i<=9; i++){  
+  y=n*i;
+  console.log(`${n} x ${i} = ${y}`);
+}
+}
+bangCuChuong(9);
